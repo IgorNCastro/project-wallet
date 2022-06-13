c91302f013cc8cfa15150e3651851edee8e31fbe
Igor Castro <ncastro.igor@gmail.com>
2022-06-13 16:05:59 -0300

    Updates

---
+++ precache-manifest.24ba97690b34a060abc4170dce76f40b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4f2b6548b5937cb3de04d490fcfa7a",
+    "url": "/project-wallet/index.html"
+  },
+  {
+    "revision": "ac97d62ab1b7e782a6ab",
+    "url": "/project-wallet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "99c46489885a78bb1861",
+    "url": "/project-wallet/static/js/2.ad3354ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-wallet/static/js/2.ad3354ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac97d62ab1b7e782a6ab",
+    "url": "/project-wallet/static/js/main.f0251115.chunk.js"
+  },
+  {
+    "revision": "ec535ca5a784f5e1851a",
+    "url": "/project-wallet/static/js/runtime-main.2e7da562.js"
+  }
+]);
